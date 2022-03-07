9d8ba1fdb52dd234f348f8ec8bdfef97e4e436ea
Abdulla Sakallah <iaboodws11@gmail.com>
2022-03-06 22:35:53 -0600

    Create driver.c

---
+++ driver.c
@@ -0,0 +1,52 @@
+// Reads a string for a serialized RB tree, deserializes it,
+// performs some insertions, then serializes the revised tree.
+
+//Name: Abdulla Sakallah                                         Student ID: 1001846204
+// Compiling instructions on Omega:-
+//==================================
+// gcc driver.c
+// ./a.out < a.txt
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "RB.c"
+
+int main()
+{
+    int inputBytes;
+    char *inputString, *outputString;
+    char formatString[100];
+    int insertKeys, i, key;
+
+    scanf("%d", &inputBytes);
+
+    inputString = (char *)malloc(inputBytes);
+    if (!inputString)
+    {
+        printf("malloc failed %d\n", __LINE__);
+        exit(0);
+    }
+    sprintf(formatString, "%%%ds", inputBytes);
+    scanf(formatString, inputString);
+
+    STinit();
+    STdeserialize(inputString);
+    free(inputString);
+
+    scanf("%d", &insertKeys);
+    for (i = 0; i < insertKeys; i++)
+    {
+        scanf("%d", &key);
+        STinsert(key);
+    }
+
+
+    outputString = (char*) malloc(sizeof(char)*400);
+
+    STserialize(getHead(),outputString);
+
+    printf("%lu %s\n", strlen(outputString) + 1, outputString);
+    free(outputString);
+    freeMemory(getHead());
+}
+
